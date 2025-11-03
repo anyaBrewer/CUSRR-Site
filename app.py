3eaf83092454f5d9e563c8bfe59884865f0e9e51
Lucas <lplefe26@colby.edu>
2025-11-03 09:10:13 -0500

    Improved File Structure

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def program():
+    return render_template('organizer.html')
+
+@app.route('/organizer')
+def organizer():
+    return render_template('organizer.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('organizer.html')
+
+@app.route('/attendees')
+def attendees():
+    return render_template('organizer.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
